d70ed9998f95dbd42670dd72ffa4211c9b284305
Victor Savu <victorsavu3@gmail.com>
2014-02-06 14:39:47 +0200

    fix RecordArray

--- core/model/recordArray.js
+++ core/model/recordArray.js
@@ -1,5 +1,7 @@
 DS.RecordArray = Ember.ArrayProxy.extend({
   init: function() {
+    this._super();
+
     var store = this.get('store');
     var filter = this.get('recordFilter');
     var type = this.get('type').typeKey;
@@ -9,14 +11,12 @@ DS.RecordArray = Ember.ArrayProxy.extend({
     store.on('record:' + type + ':create', function(record) {
       if((_.isUndefined(filter) || filter(self, record)) && !self.contains(record)) {
         self.pushObject(record);
-        self.notifyPropertyChange('content');
       }
     });
 
     store.on('record:' + type + ':destroy', function(record) {
       if(self.contains(record)) {
         self.removeObject(record);
-        self.notifyPropertyChange('content');
       }
     });
   }
